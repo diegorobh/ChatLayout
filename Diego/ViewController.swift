38d08c9587d9d94cf454be0965ba8a8c92455ac5
diegoparedes <diegorobh@gmail.com>
2019-10-15 14:45:50 -0500

    Initial Commit

---
+++ Diego/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Diego
+//
+//  Created by diego paredes on 15/10/19.
+//  Copyright © 2019 diego paredes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
